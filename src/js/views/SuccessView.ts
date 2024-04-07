b556b823fe7e8a183b5dc9b76b377794bf32b3cc
Aleksey Orekhov <aleoreh@gmail.com>
2024-04-07 10:20:11 +0300

    Исправляет неточность в реализации задания

--- src/js/views/SuccessView.ts
+++ src/js/views/SuccessView.ts
@@ -1,12 +1,18 @@
 import { ensureElement } from '../utils/utils';
 import { AppView } from './AppView.base';
 
+interface ISuccessViewModel {
+	description: string;
+}
+
 interface ISuccessViewEvents {
 	onClose: () => void;
 }
 
-export class SuccessView extends AppView<undefined> {
+export class SuccessView extends AppView<ISuccessViewModel> {
 	private _closeButton: HTMLButtonElement;
+	private _description: HTMLElement;
+
 	constructor(events: ISuccessViewEvents) {
 		super('successTemplate');
 
@@ -17,5 +23,14 @@ export class SuccessView extends AppView<undefined> {
 		this._closeButton.addEventListener('click', () => {
 			events.onClose();
 		});
+
+		this._description = ensureElement<HTMLElement>(
+			'.order-success__description',
+			this.container
+		);
+	}
+
+	set description(value: string) {
+		this.setText(this._description, value);
 	}
 }
