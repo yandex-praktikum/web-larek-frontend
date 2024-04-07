c4885ee79120e918d89c4e5f9b72f71a5033bda3
Aleksey Orekhov <aleoreh@gmail.com>
2024-04-07 10:20:37 +0300

    Исправляет нарушение принципов SOLID

--- src/js/services/product.service.ts
+++ src/js/services/product.service.ts
@@ -1,14 +1,14 @@
 import { IProductService } from '../app/ports';
 import { Product } from '../models';
 import { API_URL, CDN_URL } from '../utils/constants';
-import { WebLarekApi } from './webLarekApi.service';
+import { WebLarekApi, getApi } from './webLarekApi.service';
 
 // TODO: передавать api в конструктор
 export class ProductService implements IProductService {
 	private _api: WebLarekApi;
 
 	constructor() {
-		this._api = new WebLarekApi(CDN_URL, API_URL);
+		this._api = new WebLarekApi(getApi(API_URL), CDN_URL);
 	}
 
 	getProducts: () => Promise<Product[]> = () => {
